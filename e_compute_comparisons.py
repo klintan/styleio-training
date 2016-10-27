2ecc157c481d20f8c45047c00729b1c8fe73f95c
Andreas Klintberg <andreas.klintberg@meltwater.com>
2016-10-26 23:06:20 -0700

    starting refactoring image comparison computation

--- e_compute_comparisons.py
+++ e_compute_comparisons.py
@@ -13,57 +13,77 @@ import cPickle
 import imtools
 import numpy as np
 
-f = open(sys.argv[1]+'_histograms.pickle', 'r')
-histograms = pickle.load(f)
-f.close()
+class ImageSimilarity():
+    def __init__(self, path):
+        self.path = path
+        self.histograms = None
+        self.im_list = imtools.get_imlist(path)
+        self.all_scores=[]
+        self.load_histograms()
+        self.logger = logging.getLogger(__name__)
 
-all_scores=[]
+    def load_histograms(self)
+            self.logger.info('Loading histograms...')
+            try:
+                f = open(self.path+'_histograms.pickle', 'r')
+                self.histograms = pickle.load(f)
+                f.close()
+                self.logger.info('Finished loading histograms')
 
-f1 = open(sys.argv[1]+'_tfidf.pickle', 'r')
-while 1:
-    try:
-        all_scores.append(pickle.load(f1))
-    except EOFError:
-        break
-f1.close()
+            except Exception as e:
+                self.logger.error('Failed to load histograms %s', e)
 
-#compute new histograms with weights
-for idx,hist in enumerate(histograms):
-    #normalize the histogram
-    #hist = hist/float(sum(hist))
+    def load_tfidf(self)
+            self.logger.info('Loading TfIdf...')
+            try:
+                f = open(self.path+'_tfidf.pickle', 'r')
+                self.histograms = pickle.load(f)
+                f.close()
+                self.logger.info('Finished loading TfIdf')
 
-    #TFIDF weighing
-    hist = hist*all_scores[idx]
+            except Exception as e:
+                self.logger.error('Failed to load TfIdf %s', e)
 
+    def tfidf_weighing(self, histograms):
+        #compute new histograms with weights
+        for idx,hist in enumerate(histograms):
+            #normalize the histogram
+            #hist = hist/float(sum(hist))
 
-path = sys.argv[1]
-im_list = imtools.get_imlist(path)
+            #TFIDF weighing
+            hist = hist*all_scores[idx]
 
+    def compute_image_similarity(self, histograms):
+        dist_function = dist.distance('chisquared')
+        temp_dist = []
+        all_dist = []
+        #print dist_function
+        for idx,h1 in enumerate(histograms):
+            f1 = open(sys.argv[1]+'_comparison.pickle', mode='a+b')
+            for h2 in histograms:
+                vector_distance = dist_function.compute_distance(h1,h2)
+                #print type(vector_distance)
+                temp_dist.append(vector_distance)
+                #print type(temp_dist)
 
-dist_function = dist.distance('chisquared')
-temp_dist = []
-all_dist = []
+            #all_dist.append(temp_dist)
+            cPickle.dump(temp_dist,f1)
 
+            print idx
 
+            #print temp_dist
+            #print np.shape(all_dist)
+            temp_dist = []
+            f1.close()
 
-#print dist_function
-for idx,h1 in enumerate(histograms):
-    f1 = open(sys.argv[1]+'_comparison.pickle', mode='a+b')
-    for h2 in histograms:
-        vector_distance = dist_function.compute_distance(h1,h2)
-        #print type(vector_distance)
-        temp_dist.append(vector_distance)
-        #print type(temp_dist)
+# f1 = open(sys.argv[1]+'_tfidf.pickle', 'r')
+# while 1:
+#     try:
+#         all_scores.append(pickle.load(f1))
+#     except EOFError:
+#         break
+# f1.close()
 
-    #all_dist.append(temp_dist)
-    cPickle.dump(temp_dist,f1)
-
-    print idx
-
-    #print temp_dist
-    #print np.shape(all_dist)
-    temp_dist = []
-    f1.close()
-
-print all_dist
-print np.shape(all_dist)
+if __name__ == '__main__':
+image_similarity = ImageSimilarity(path=sys.argv[1])
+image_similarity.compute_image_similarity()
