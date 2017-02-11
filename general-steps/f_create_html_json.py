d0cd50dd846c6d88d4de922db9eb4e6f7a1474bc
Andreas Klintberg <andreas.klintberg@meltwater.com>
2017-02-10 23:27:09 -0800

    converted html generation to use jinja2 templating engine

--- general-steps/f_create_html_json.py
+++ general-steps/f_create_html_json.py
@@ -10,9 +10,10 @@ import imtools
 import pickle
 import cPickle
 import numpy as np
-from jinja2 import Template, Environment, PackageLoader
+from jinja2 import Template, Environment, PackageLoader, FileSystemLoader
 import logging
 import yaml
+import os
 
 logging.basicConfig(level=logging.INFO)
 
@@ -26,17 +27,17 @@ class EvaluationFramework():
         self.logger = logging.getLogger(__name__)
         self.html= None
         self.all_dist = []
-
+        self.all_data = []
         #jinja 2
-        self.template = None
-        #self.env = Environment(loader=PackageLoader('yourapplication', 'templates'))
+        template_dir = os.path.dirname(os.path.abspath(__file__))
+        env = Environment(loader=FileSystemLoader(template_dir + "/templates"),trim_blocks=True)
+        self.template = env.get_template('annotate.html')
         self.load_comparison()
 
 
     def load_comparison(self):
         self.logger.info('Loading comparison scores...')
         f = open(self.path+'_comparison.pickle', 'r')
-        #all_dist = pickle.load(f)
         while 1:
             try:
                 self.all_dist.append(pickle.load(f))
@@ -46,45 +47,42 @@ class EvaluationFramework():
         self.logger.info('Succesfully loaded comparison scores')
 
     def generate_html(self):
-        #jinja 2
-        # template = env.get_template('layout.html')
-        # self.template = Template('Hello {{ name }}!')
-        # template.render(name='John Doe')
-        # old
         self.logger.info('Generate HTML...')
-        self.html = '<html><head></head><body>'
         json = []
         for idx,dist in enumerate(self.all_dist):
             sort_index = np.argsort(dist)
             top20 = sort_index[0:20]
             json.append({"img":"","similar":[]})
             img_index = str(idx)
-            self.html+="<p>"+str(idx)+"</p>"
+            img_row = []
             for idy,image in enumerate(top20):
-
                 if idy == 0:
                     json[idx]['img'] = self.im_list[image];
                     continue
 
-                self.html += '<img src='+ self.im_list[image]+' width="'+str(100)+'" height="'+str(100)+'">'
-                img_source = self.im_list[image]
-                #self.html += '<p>Score:'+ str(dist[image])+'</p>'
-                json[idx]['similar'].append({"img":self.im_list[image],"pattern": str(dist[image]),"color":""})
+                img_path = os.path.normpath(self.im_list[image])
+                img_source = "/".join(img_path.split(os.sep)[-2:])
+                img_row.append({"src":img_source, "score":str(dist[image])})
 
-            self.html += '<hr>'
-            if idx == 1000:
+                #self.html += '<img src='+ self.im_list[image]+' width="'+str(100)+'" height="'+str(100)+'">'
+                #img_source = self.im_list[image]
+                #self.html += '<p>Score:'+ str(dist[image])+'</p>'
+                #json[idx]['similar'].append({"img":self.im_list[image],"pattern": str(dist[image]),"color":""})
+
+            self.all_data.append(img_row)
+            if idx == 500:
                 break
-        self.html += '</body></html>'
-        self.save_html()
+
+        self.render_html()
 
     def save_html(self):
-        f1 = open(self.path+'.html', 'wb')
-        cPickle.dump(self.html,f1)
-        f1.close()
+        with open(self.path+'.html', 'wb') as f1:
+            cPickle.dump(self.html, f1)
+
+    def render_html(self):
         # jinja2
-        #self.template.render(the='variables', go='here')
-
-
+        with open(self.path+'_jinja.html', 'wb') as f2:
+            cPickle.dump(self.template.render(all_images=self.all_data).encode( "utf-8" ) ,f2)
 
 if __name__ == '__main__':
     ef = EvaluationFramework()
---
+++ general-steps/templates/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('Application', []);
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{a');
+  $interpolateProvider.endSymbol('a}');
+}]);
